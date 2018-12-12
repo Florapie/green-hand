51d501ab9d4b79e0ddded4ef38f0f524608cc1d2
florapie <xdliu09@163.com>
2018-12-12 10:12:31 +0800

    re-write 14-16
    14: define one main function, directly call main().
    15&16: defene read(json_r) and write(json_w) function seperately, use if __name__=='__main__' to call

--- fifteen.py
+++ fifteen.py
@@ -6,31 +6,33 @@ import pandas as pd
 import numpy as np
 import xlwt
 
-def json_r():
-#open("D:\Intern\PY\Fourteen.txt",'r',encoding='utf8') as File:
-    with open("D:\Intern\PY\city.txt",'r',encoding='utf8') as File:
-        f=File.read()
-    print(f)
-    print(type(f))
-    jsonFile=json.load(open(r'D:\Intern\PY\city.txt'))
-    print(jsonFile)
-    print(type(jsonFile))
+fileDir='D:\Intern\PY'
+
+def json_r(fileName,fileDir='D:\Intern\PY'):
+    
+    if os.path.isfile(os.path.join(fileDir,fileName)):
+        jsonFile=json.load(open(os.path.join(fileDir,fileName)))
+        print(jsonFile)
+        print(type(jsonFile))
+    else:
+        print("%s doesn't exist!" %fileName)
+    return jsonFile
+
+def json_w(jsonFile,xlsName='city15.xls',sheetName='One'):
+    newWorkbook=xlwt.Workbook()  #create excel
+    newSheet=newWorkbook.add_sheet(sheetName)  #creat a sheet
 
     key=list(jsonFile.keys())
     value=list(jsonFile.values())
-    myWorkbook=xlwt.Workbook()  #create excel 
-    mySheet=myWorkbook.add_sheet("json_string")   #creat a sheet
-    #for i in range(len(list(jsonFile.keys())):
+
     for i in range(len(jsonFile)):
         k1=key[i]
         k2=value[i]
-        mySheet.write(i,0,k1)
-        mySheet.write(i,1,k2)
-
-    myWorkbook.save('D:\Intern\PY\city.xls')
-
+        newSheet.write(i,0,k1)
+        newSheet.write(i,1,k2)
+    newWorkbook.save(os.path.join(fileDir,xlsName))
 
 
 if __name__=="__main__":
-    json_r()
+    json_w(json_r('city15.txt'))
 
--- sixteen.py
+++ sixteen.py
@@ -2,14 +2,36 @@ import os
 import xlwt
 import json
 
-def numbs_w():
-    with open('D:\Intern\PY\shuzi.txt','r',encoding='utf8') as File:
-        f=File.read()
-    print(f)
-    jsonFile=json.loads(f)
+fileDir='D:\Intern\PY'
+def json_r(fileName='number16.txt'):
+    with open(os.path.join(fileDir,fileName),'r') as file:
+        jsonFile=json.load(file)
     
     print(jsonFile)
+    print(type(jsonFile))
+
+    return(jsonFile)
+
+def json_w(jsonFile,fileName='number16.xls'):
+    newWorkbook=xlwt.Workbook()
+    newSheet=newWorkbook.add_sheet('One')
+
+    for i in range(len(jsonFile)):
+        k=jsonFile[i]
+        for j in range(len(k)):
+            k1=k[j]
+            newSheet.write(i,j,k1)
+    newWorkbook.save(os.path.join(fileDir,fileName))
+
+
+# def numbs_w():
+#     with open('D:\Intern\PY\shuzi.txt','r',encoding='utf8') as File:
+#         f=File.read()
+#     print(f)
+#     jsonFile=json.loads(f)
+    
+#     print(jsonFile)
 
 
 if __name__=='__main__':
-    numbs_w()
+    json_w(json_r())
